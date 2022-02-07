2518a92c2cbaaf95726979ac6f33792c62bdf5c6
Monjurul <raju.monjurulkarim@gmail.com>
2022-02-07 14:14:01 -0500

    Optional argument commented out

--- scripts/extract.py
+++ scripts/extract.py
@@ -12,6 +12,7 @@ input_dir = './input/' #Directory that contains the videos, you
 destination_dir = './output/' #destination directory where extracted frames
                                 # will be stored. For each video it will create
                                 # separate subfolder by each video's name
-dim = (1080,720) #optional
+# dim = (1080,720) #optional
 
-FrameExtract(input_dir,destination_dir,dim)
+# FrameExtract(input_dir,destination_dir,dim)
+FrameExtract(input_dir,destination_dir)
