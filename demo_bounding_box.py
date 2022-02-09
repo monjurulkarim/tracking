8d46bf8a48cda605cf9108619971bcc27fdd479a
Muhammad Monjurul Karim <40798690+monjurulkarim@users.noreply.github.com>
2022-02-09 11:04:11 -0500

    bug fixed

--- demo_bounding_box.py
+++ demo_bounding_box.py
@@ -47,6 +47,12 @@ def get_args():
     return args
 
 
+def trajectory():
+
+    return
+
+
+
 def main():
     color_list = [
         [255, 0, 0],  # blue
@@ -95,8 +101,8 @@ def main():
                         risk2 = value[2]
                         risk2 = re.sub("[()]","", risk2)
                         risk2 = [int(i) for i in risk2.split(',')]
-                        cv2.rectangle(img, ristructuresk2, color_list[1], thickness=2)
-                    if value[3] != '':image_folder
+                        cv2.rectangle(img, risk2, color_list[1], thickness=2)
+                    if value[3] != '':
                         risk3 = value[3]
                         risk3 = re.sub("[()]","", risk3)
                         risk3 = [int(i) for i in risk3.split(',')]
@@ -119,80 +125,6 @@ def main():
 
     return
 
-# 
-# def main_original():
-#     args = get_args()
-#     input_video = args.input_video
-#     bbox_dir = args.bbox_dir
-#     folder_name = input_video.split('/')[-1]
-#     video_name = f"{folder_name}.mp4"
-#     print(video_name)
-#     bbox_csv = f"{bbox_dir}{folder_name}.csv"
-#     color_list = [
-#         [255, 0, 0],  # blue
-#         [0, 0, 255], #red
-#         [0, 255, 0], #lime
-#         [128, 0, 128],  # purple
-#         [0, 0, 255],  # red
-#         [255, 0, 255],  # fuchsia
-#         [0, 128, 0],  # green
-#         [128, 128, 0],  # teal
-#         [0, 0, 128],  # maroon
-#         [0, 128, 128],  # olive
-#         [0, 255, 255],  # yellow
-#     ]
-#
-#     images = natsorted([img for img in os.listdir(input_video) if img.endswith(".jpg")])
-#     height, width = 720, 1080
-#
-#     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-#     video = cv2.VideoWriter(video_name, fourcc, 20, (width, height))
-#     print('======================')
-#     print(f"{video_name}...")
-#
-#
-#
-#     with open(bbox_csv,'r', newline = '') as csvfile:
-#         reader = csv.reader(csvfile,delimiter=',')
-#         heading = next(reader)
-#         value = next(reader)
-#
-#         for i in range(100):
-#             img = cv2.imread(os.path.join(input_video, images[i]))
-#             if i< int(value[0]):
-#                 video.write(img)
-#             elif i == int(value[0]):
-#                 risk1 = value[1]
-#                 risk1 = re.sub("[()]","", risk1)
-#                 risk1 = [int(i) for i in risk1.split(',')]
-#                 cv2.rectangle(img, risk1, color_list[0], thickness=2)
-#                 if value[2] != '':
-#                     risk2 = value[2]
-#                     risk2 = re.sub("[()]","", risk2)
-#                     risk2 = [int(i) for i in risk2.split(',')]
-#                     cv2.rectangle(img, risk2, color_list[1], thickness=2)
-#                 if value[3] != '':
-#                     risk3 = value[3]
-#                     risk3 = re.sub("[()]","", risk3)
-#                     risk3 = [int(i) for i in risk3.split(',')]
-#                     cv2.rectangle(img, risk3, color_list[2], thickness=2)
-#                 if value[4] != '':
-#                     risk4 = value[4]
-#                     risk4 = re.sub("[()]","", risk4)
-#                     risk4 = [int(i) for i in risk4.split(',')]
-#                     cv2.rectangle(img, risk4, color_list[3], thickness=2)
-#                 try:
-#                     video.write(img)
-#                     value = next(reader)
-#                 except:
-#                     print('Finished operation')
-#             else:
-#                 video.write(img)
-#
-#         cv2.destroyAllWindows()
-#         video.release()
-#
-#     return
 
 
 if __name__ == '__main__':
